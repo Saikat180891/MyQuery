b8a60e193e27c6d439e8a8c12c375b76f5a30a0e
saikat <saptadipa123@gmail.com>
2019-06-16 01:50:28 +0530

    commit 4

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 const navbar = new MyQuery("div");
 navbar.addClasses("navbar", "flex-between", "v-center").addParent("body");
 
-const brand = new MyQuery("div")
+const brand = new MyQuery("div");
 brand.addClasses("brand", "flex-center", "v-center").addText("Brand");
 
 const rightControls = new MyQuery("div");
 rightControls.addClasses("brand");
 
-
 const list = new MyQuery("ul");
-list.addClasses("list").on("click", (e)=>{
+list.addClasses("list").on("click", e => {
   console.log(e.target);
-})
+});
 
-const myArray = [1, 2, 3, 4, 5, 6, 7, 8]
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
 myArray.forEach(ele => {
   const listItem = new MyQuery("li");
   listItem.addClasses("listitem").addText("Hello " + ele);
   list.insertInnerElement(listItem);
-})
+});
 
-
-console.log(list)
+console.log(list);
 
 navbar
   .insertInnerElement(brand)
   .insertInnerElement(rightControls)
-  .insertAfter(list)
+  .insertAfter(list);
--- framework.js
+++ framework.js
@@ -2,26 +2,26 @@ function MyQuery(type) {
   this.element = document.createElement(type);
 }
 
-MyQuery.prototype.addParent = function (parent) {
+MyQuery.prototype.addParent = function(parent) {
   document.querySelector(parent).appendChild(this.element);
   return this;
 };
 
-MyQuery.prototype.setAttributes = function (attributes) {
+MyQuery.prototype.setAttributes = function(attributes) {
   for (let key in attributes) {
     this.element.setAttribute(key, attributes[key]);
   }
   return this;
 };
 
-MyQuery.prototype.addClasses = function (...classes) {
+MyQuery.prototype.addClasses = function(...classes) {
   classes.forEach(clas => {
     this.element.classList.add(clas);
   });
   return this;
 };
 
-MyQuery.prototype.insertInnerElement = function (node) {
+MyQuery.prototype.insertInnerElement = function(node) {
   this.element.appendChild(node.element);
   return this;
 };
@@ -30,29 +30,34 @@ MyQuery.prototype.addText = function(text) {
   let textNode = document.createTextNode(text);
   this.element.appendChild(textNode);
   return this;
-}
+};
 
 MyQuery.prototype.addInnerHTML = function(html) {
   this.element.innerHTML = html;
   return this;
-}
+};
 
-MyQuery.prototype.on = function (event, callback) {
+MyQuery.prototype.on = function(event, callback) {
   this.element.addEventListener(event, callback);
   return this;
 };
 
-MyQuery.prototype.append = function (where, node) {
+MyQuery.prototype.append = function(where, node) {
   this.element.insertAdjacentHTML(where, node.element);
   return this;
 };
 
-MyQuery.prototype.insertAfter = function (node) {
+MyQuery.prototype.insertAfter = function(node) {
   this.element.after(node.element);
   return this;
 };
 
-MyQuery.prototype.createDropdown = function (value = [], viewValue = [], event, callback) {
+MyQuery.prototype.createDropdown = function(
+  value = [],
+  viewValue = [],
+  event,
+  callback
+) {
   if (viewValue.length !== value.length) {
     throw "Length of arg1 not equal to length of arg2";
   }
@@ -69,3 +74,48 @@ MyQuery.prototype.createDropdown = function (value = [], viewValue = [], event, 
   this.element.appendChild(this.select);
   return this;
 };
+
+function $(selector) {
+  const self = {};
+  self.selector = selector;
+  self.element = document.querySelector(selector);
+  self.text = function(text) {
+    self.element.innerText = text;
+    return self;
+  };
+  self.html = function(HTML) {
+    self.element.innerHTML = HTML;
+    return self;
+  };
+  self.css = function(property, value) {
+    self.element.style[property] = value;
+    return self;
+  };
+
+  self.addClass = function(className) {
+    self.element.classList.add(className);
+    return self;
+  };
+  self.removeClass = function(className) {
+    self.element.classList.remove(className);
+    return self;
+  };
+  self.on = function(event, callback) {
+    self.element.addEventListener(event, callback);
+    return self;
+  };
+  self.value = function() {
+    return self.element.target.value;
+  };
+  self.append = function(elem) {
+    return self.element.appendChild(elem);
+  };
+  self.attr = function(name, value) {
+    if (!value) {
+      return self.element.getAttribute(name);
+    }
+    self.element.setAttribute(name, value);
+    return self;
+  };
+  return self;
+}
